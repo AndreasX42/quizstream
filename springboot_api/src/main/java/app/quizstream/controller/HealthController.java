bff9be3669d19e8d4f29e8cd6b92de35979134e7
AndreasX42 <andreas.pangerl.42@gmail.com>
2025-04-03 07:17:22 -0300

    add import

--- springboot_api/src/main/java/app/quizstream/controller/HealthController.java
+++ springboot_api/src/main/java/app/quizstream/controller/HealthController.java
@@ -1,5 +1,7 @@
 package app.quizstream.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
